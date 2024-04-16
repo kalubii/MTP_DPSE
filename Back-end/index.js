5d51a86d0d8ac4de71c658e05515f4f7016d0459
Kalubii <nambinintsoa225@gmail.com>
2024-04-16 11:53:05 +0200

    Liste des travaux Récent OK

--- Back-end/index.js
+++ Back-end/index.js
@@ -33,7 +33,6 @@ app.get('/projet2022', (req, res) => {
     LEFT JOIN identification i ON a.id_identification = i.id_identification
     LEFT JOIN localisation l ON i.id_localisation = l.id_localisation
     LEFT JOIN suividae s ON i.id_identification = s.id_identification
-    LEFT JOIN total_general t ON i.id_total_general = t.id_total_general
     WHERE 1=1    
     `;
 
@@ -203,254 +202,38 @@ rows.forEach(row => {
  
 });
 
+app.get('/travauxRecents',(req,res) => {
+  const sql = `
+  SELECT id_localisation, REGIONS_CONCERNEES, PK_DEBUT, PK_FIN, ACTIVITES, SITUATION, Financement,Co_t_Ar_
+  FROM (
+    (SELECT id_localisation, REGIONS_CONCERNEES, PK_DEBUT, PK_FIN, NULL AS activites, NULL AS financement, NULL AS situation, NULL AS co_t_Ar_ FROM localisation
+    ORDER BY id_localisation DESC LIMIT 7)
+    UNION ALL
+    (SELECT id_identification, NULL AS regions_concernees, NULL AS pk_debut, NULL AS pk_fin, ACTIVITES, Financement, NULL AS situation, NULL AS co_t_Ar_ FROM identification
+    ORDER BY id_identification DESC LIMIT 7)
+    UNION ALL
+    (SELECT id_caracteristique, NULL AS regions_concernees, NULL AS pk_debut, NULL AS pk_fin, NULL AS activites,NULL AS financement, NULL AS situation, Co_t_Ar_ FROM caracteristique
+     ORDER BY id_caracteristique DESC LIMIT 7)
+    UNION ALL
+    (SELECT id_avancement, NULL AS regions_concernees, NULL AS pk_debut, NULL AS pk_fin, NULL AS activites,NULL AS financement, SITUATION, NULL AS co_t_Ar_ FROM avancement
+     ORDER BY id_avancement DESC LIMIT 7)
+  ) AS subquery
+  ORDER BY id_localisation DESC
+  LIMIT 7`;
+ 
+ 
+
+      db.query(sql, (err, results) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          console.log(results)
+          res.json(results);
+        }
+      });
+})
 
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(Server running on port ${PORT}));
-
-// REQUETE LOCALITE
-
-// app.get('/localite_RN', (req, res) => {
-//     // Extraction des paramètres de requête
-    // const faritany = req.query.faritany;
-    // const regions = req.query.region ? req.query.region.split(',') : [];
-    // const axes = req.query.axe ? req.query.axe.split(',') : [];
-    // const pkDebut = req.query.pkDebut;
-    // const pkFin = req.query.pkFin;
-
-//     // console.log('Faritany:', faritany);
-   
-    // let sql = 'SELECT * FROM localite WHERE 1=1';
-
-    // if(faritany){
-    // sql += ` AND FARITANY = '${faritany}'`;
-    // }
-
-    // if (regions.length > 0) {
-    //    sql += ` AND REGION IN (${regions.map(region => `'${region}'`).join(',')})`;
-    // }
-    // if (axes.length > 0) {
-    //    sql += ` AND AXES IN (${axes.map(axe => `'${axe}'`).join(',')})`;
-    // }
-    // if (pkDebut) {
-    //    sql += ` AND PK_DEBUT >= ${pkDebut}`;
-    // }
-    // if (pkFin) {
-    //    sql += ` AND PK_FIN <= ${pkFin}`;
-    // }
-
-    //  db.query(sql, (err, results) => {
-    //     if (err) {
-    //         res.status(500).send(err);
-    //     } else {
-    //         res.json(results);
-    //     }
-//     });
-// });
-
-// //REQUETE PROJET
-// app.get('/projet',(req,res)=>{
-
-    // const idRef = req.query.idRef;
-    // const date = req.query.date;
-    // const regions = req.query.region;
-    // const axes = req.query.axe;
-    // const pkDebut = req.query.pkDebut;
-    // const pkFin = req.query.pkFin;
-
-    // console.log('id',idRef)
-   
-    // let sql = 'SELECT * FROM projet WHERE 1=1';
-
-    // if(idRef){
-    //     sql+= ` AND idProjet = '${idRef}'`;
-    // }
-
-    // if(date){
-    //     sql+= ` AND _date = '${date}'`;
-    // }
-//     if (regions) {
-//        sql += ` AND REGIONS_CONCERNEES = '${regions}'`;
-//     }
-//     if (axes) {
-//        sql += ` AND AXE = '${axes}'`;
-//     }
-//     if (pkDebut) {
-//        sql += ` AND PK_DEBUT >= ${pkDebut}`;
-//     }
-//     if (pkFin) {
-//        sql += ` AND PK_FIN <= ${pkFin}`;
-//     }
-
-//      db.query(sql, (err, results) => {
-//         if (err) {
-//             res.status(500).send(err);
-//         } else {
-//             res.json(results);
-//         }
-//     });
-// });
-
-
-// app.get('/liste_RN/:axe/:region/:pk_debut/:pk_fin',(req,res) => {
-//     let sql = "SELECT * FROM localite WHERE region = ?";
-//     let params = [req.params.region];
-
-//     if (req.params.axe) {
-//         sql += " AND AXES = ?";
-//         params.push(req.params.searchAxe);
-//     }
-//     if (req.params.pk_debut) {
-//         sql += " AND PK_DEBUT = ?";
-//         params.push(req.params.pk_debut);
-//     }
-//     if (req.params.pk_fin) {
-//         sql += " AND PK_FIN = ?";
-//         params.push(req.params.pk_fin);
-//     }
-//     if (req.params.region) {
-//         sql += " AND REGION = ?";
-//         params.push(req.params.region);
-//     }
-
-//     db.query(sql, params, (err, result) => {
-//         if (err) return res.json({ Message: err });
-//         return res.json(result);
-//     })
-
-// // })
-
-// app.get('/localite_RN', (req, res) =>{
-//     let params = []
-//     let sql = "SELECT * from localite"
-
-//     if (req.query.axe) {
-//         sql += " AND AXES = ?";
-//         params.push(req.query.axe);
-//     }
-
-//     if (req.query.pkFin) {
-//         sql += " AND PK_FIN = ?";
-//         params.push(req.query.pkFin);
-//     }
-
-//     if (req.query.pkDebut) {
-//         sql += " AND PK_DEBUT = ?";
-//         params.push(req.query.pkDebut);
-//     }
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:err})
-//         return res.json(result);
-//     })
-
-// })
-// app.get('/localiteAntsiranana', (req, res) =>{
-//     const sql = "SELECT * FROM antsiranana"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-
-// })
-// app.get('/localiteFianarantsoa', (req, res) =>{
-//     const sql = "SELECT * FROM fianarantsoa"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-
-// })
-// app.get('/localiteMahajanga', (req, res) =>{
-//     const sql = "SELECT * FROM mahajanga"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-
-// })
-// app.get('/localiteToamasina', (req, res) =>{
-//     const sql = "SELECT * FROM toamasina"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-
-// })
-// app.get('/localiteToliara', (req, res) =>{
-//     const sql = "SELECT * FROM toliara"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-
-// })
-
-// REQUETE PROJET
-
-//    if(req.params.searchAxe != "null" && req.params.pkDebut != "null" && req.params.pkFin != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' AXE= '"+req.params.searchAxe+"' PK_DEBUT ='"+req.params.pkDebut+"' PK_FIN ='"+req.params.pkFin+"'"
-//    }else if(req.params.searchAxe != "null" && req.params.pkDebut != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= "+req.params.annee+"' AXE= '"+req.params.searchAxe+"' PK_DEBUT ='"+req.params.pkDebut+"'"
-//    }else if(req.params.searchAxe != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+" AXE= '"+req.params.searchAxe+"'"
-//    }else if(req.params.searchAxe != "null" && req.params.pkFin != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' AXE= '"+req.params.searchAxe+"' PK_FIN ='"+req.params.pkFin+"'"
-//    }else if(req.params.pkDebut != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' PK_DEBUT ='"+req.params.pkDebut+"'"
-//    } else if(req.params.pkFin != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' PK_FIN ='"+req.params.pkFin+"'"
-//    }else if (req.params.pkDebut != "null" && req.params.pkFin != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' PK_DEBUT ='"+req.params.pkDebut+"' PK_FIN ='"+req.params.pkFin+"'"
-//    }else if(req.params.searchAxe != "null" && req.params.pkFin != "null"){
-//     sql =  "SELECT * FROM localite WHERE _date= '"+req.params.annee+"' AXE= '"+req.params.searchAxe+"' PK_FIN ='"+req.params.pkFin+"'"
-//    }
-
-// (sql, (err,result)=>{
-//     if(err) return res.json({Message:err})
-//     return res.json(result);
-// })
-
-// app.get('/projet2022', (req, res) =>{
-//     const sql = "SELECT * FROM donnee"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-// })
-
-// app.get('/projet2021', (req, res) =>{
-//     const sql = "SELECT * FROM projet2021"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-// })
-
-// app.get('/projet2020', (req, res) =>{
-//     const sql = "SELECT * FROM projet2020"
-//     db.query(sql, (err,result)=>{
-//         if(err) return res.json({Message:"Erreur de serveur"})
-//         return res.json(result);
-//     })
-// })
 app.listen(8081, ()=>{
     console.log('Serveur démarrer avec succès...')
 })
-
-//INSERET PROJET
-app.post('/projet2024', (req, res) => {
-    let { faritany, region, commune, axe, section, titre, programmelf, conventionlf, conventionliblf, activite, cout, naturesurface, longueur, pk_debut, pk_fin, loc_debut, loc_Fin, annee, avancement, observation } = req.body;
-
-    db.query(
-        "INSERT INTO projet2024 (faritany, region, commune, axe, section, titre, programmelf, conventionlf, conventionliblf, activite, cout, naturesurface, longueur, pk_debut, pk_fin, loc_debut, loc_fin, annee, avancement, observation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [faritany, region, commune, axe, section, titre, programmelf, conventionlf, conventionliblf, activite, cout, naturesurface, longueur, pk_debut, pk_fin, loc_debut, loc_Fin, annee, avancement, observation],
-        (err, resultat) => {
-            if (err) {
-                console.error("Erreur lors de l'insertion :", err);
-                return res.status(500).json({ Message: "Erreur de serveur" });
-            }
-            console.log("Élément inséré avec succès !");
-            return res.status(200).json({ Message: "Élément inséré avec succès" });
-        }
-    );
-});
--- src/composants/Home/Drawer/Dashboard/Classification.jsx
+++ src/composants/Home/Drawer/Dashboard/Classification.jsx
@@ -6,104 +6,118 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './SousTitle';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-// Generate Order Data
-function createData(id, date, localisation, debut, fin, pkDebut,pkFin,travaux,avancement,cout) {
-  return { id, date, localisation, debut, fin, pkDebut, pkFin,travaux,avancement,cout};
-}
+// // Generate Order Data
+// function createData(id, date, localisation, debut, fin, pkDebut,pkFin,travaux,avancement,cout) {
+//   return { id, date, localisation, debut, fin, pkDebut, pkFin,travaux,avancement,cout};
+// }
 
-const rows = [
-  createData(
-    0,
-    '15 Mar, 2024',
-    'Antsiranana',
-    'Fanambana',
-    'Fanambana',
-    '182,000',
-    '194,000',
-    'Trx COLAS = RB FED 95',
-    '',
-    312.44,
-  ),
-  createData(
-    1,
-    '15 Mar, 2024',
-    'Toliara',
-    'car RN7 (stade)',
-    'car RN7 (Andranovory)',
-    '0,000',
-    '61,000',
-    '',
-    866.99,
-  ),
-  createData(
-    2, 
-    '15 Mar, 2024', 
-    'Toamasina', 
-    'Car RN2 (stade)',
-    'Aéroport Toamasina',
-    '0,000',
-    '5,000',
-    'A corriger/ decret continuité',
-    100.81),
-  createData(
-    3,
-    '15 Mar, 2024',
-    'Fianarantsoa',
-    'Car RN25 PK160',
-    'Car RN24',
-    '0,000',
-    '4,000',
-    '',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2024', 
-    'Toamasina', 
-    'Car RN2 (stade)',
-    'Aéroport Toamasina',
-    '0,000',
-    '5,000',
-    'A corriger/ decret continuité',
-    100.81
-  ),
-];
+// const rows = [
+//   createData(
+//     0,
+//     '15 Mar, 2024',
+//     'Antsiranana',
+//     'Fanambana',
+//     'Fanambana',
+//     '182,000',
+//     '194,000',
+//     'Trx COLAS = RB FED 95',
+//     '',
+//     312.44,
+//   ),
+//   createData(
+//     1,
+//     '15 Mar, 2024',
+//     'Toliara',
+//     'car RN7 (stade)',
+//     'car RN7 (Andranovory)',
+//     '0,000',
+//     '61,000',
+//     '',
+//     866.99,
+//   ),
+//   createData(
+//     2, 
+//     '15 Mar, 2024', 
+//     'Toamasina', 
+//     'Car RN2 (stade)',
+//     'Aéroport Toamasina',
+//     '0,000',
+//     '5,000',
+//     'A corriger/ decret continuité',
+//     100.81),
+//   createData(
+//     3,
+//     '15 Mar, 2024',
+//     'Fianarantsoa',
+//     'Car RN25 PK160',
+//     'Car RN24',
+//     '0,000',
+//     '4,000',
+//     '',
+//     654.39,
+//   ),
+//   createData(
+//     4,
+//     '15 Mar, 2024', 
+//     'Toamasina', 
+//     'Car RN2 (stade)',
+//     'Aéroport Toamasina',
+//     '0,000',
+//     '5,000',
+//     'A corriger/ decret continuité',
+//     100.81
+//   ),
+// ];
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
 function Orders() {
+
+  const [travauxRecent, setTravauxRecent] = useState([])
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+       try {
+         const response = await axios.get('http://localhost:8081/travauxRecents');
+         setTravauxRecent(response.data);
+       } catch (error) {
+         console.error('', error);
+       }
+    };
+    fetchSearchResults();
+   }, []);
+
   return (
     <React.Fragment>
       <Title>Travaux Récents</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Localisation</TableCell>
-            <TableCell>Début</TableCell>
-            <TableCell>Fin</TableCell>
-            <TableCell>PK Début</TableCell>
+            <TableCell>Regions</TableCell>
+            <TableCell>PK Debut</TableCell>
             <TableCell>PK Fin</TableCell>
             <TableCell>Travaux</TableCell>
-            <TableCell>Avancement</TableCell>
+            <TableCell>Financement</TableCell>
+            <TableCell>Situation</TableCell>
             <TableCell align="right">Coût (Ar)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.localisation}</TableCell>
-              <TableCell>{row.debut}</TableCell>
-              <TableCell>{row.fin}</TableCell>
-              <TableCell>{row.pkDebut}</TableCell>
-              <TableCell>{row.pkFin}</TableCell>
-              <TableCell>{row.travaux}</TableCell>
-              <TableCell>{row.avancement}</TableCell>
-              <TableCell align="right">{`${row.cout}`}</TableCell>
+          {travauxRecent.map((row,index) => (
+            <TableRow key={index}>
+              <TableCell>{row.REGIONS_CONCERNEES}</TableCell>
+              <TableCell>{row.PK_DEBUT}</TableCell>
+              <TableCell>{row.PK_FIN}</TableCell>
+              <TableCell>{row.ACTIVITES}</TableCell>
+              <TableCell>{row.Financement}</TableCell>
+              <TableCell>{row.SITUATION}</TableCell>
+              <TableCell align="right">{`${row.Co_t_Ar_}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
