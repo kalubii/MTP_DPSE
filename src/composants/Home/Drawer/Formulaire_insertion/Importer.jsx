b5dc87af03ee6770c2f457a67e8d0fb4e86f7d3f
Kalubii <nambinintsoa225@gmail.com>
2024-04-19 14:34:56 +0200

    Alert message added!

--- src/composants/Home/Drawer/Formulaire_insertion/Importer.jsx
+++ src/composants/Home/Drawer/Formulaire_insertion/Importer.jsx
@@ -2,27 +2,48 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import BackupIcon from '@mui/icons-material/Backup';
 import { Button } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 function Importer() {
   const [file, setFile] = useState();
+  const [alert, setAlert] = useState({ open: false, type: 'success', message: '' });
 
   const upload = () => {
     const formData = new FormData()
     formData.append('file',file)
     axios.post('http://localhost:8081/upload',formData)
-    .then(res => {})
-    .catch(err => console.log(err))
+    .then(res => {
+      setAlert({open: true, type:'success',message: 'Importation Réussit!'});
+      setFile(null);
+    })
+    .catch(err =>{
+      setAlert({open:true, type:'error',message:"Echec l\'importation!" })
+    })
   }
 
-  return (
+  const handleCloseAlert = () => {
+    setAlert({...alert, open:false})
+  }
+
+  return ( <>
+      <div>
+        {alert.open &&
+          <Alert severity={alert.type} onClose={handleCloseAlert} sx={{width:'fit-content'}}>
+            <AlertTitle>{alert.type === 'success' ? 'Succès' : 'Erreur'}</AlertTitle>
+            {alert.message}
+          </Alert>
+        }
+      </div>
+      
     <div  style={{display:'flex', justifyContent:'flex-end', margin:'auto'}}>
       <input type="file" onChange={(e)=> setFile(e.target.files[0])}/>
-      {/* <button className='btn btn-outline-success' type='button' onClick={upload}><BackupIcon/>Importer</button> */}
       <Button variant="contained" color="success" startIcon={<BackupIcon/>} onClick={upload}>
         Importer
       </Button>
     </div>
-  );
+
+    </>);
 }
 
 export default Importer;
